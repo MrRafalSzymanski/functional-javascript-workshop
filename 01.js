ac3ea0b947d4ce6e2c0a4bb91f60969fe17e5682
Rafal Szymanski <mr.rafal.szymanski@outlook.com>
2015-08-10 12:23:07 +0200

    Added some comments

--- 01.js
+++ 01.js
@@ -1,3 +1,18 @@
+/*
+ * FUNCTIONAL JAVASCRIPT IS GOOD
+ * ───────────────────────────────
+ * Hello World
+ * Exercise 1 of 18
+ * 
+ * # Task
+ * Write a function that takes an input string and returns it uppercased.
+ * 
+ * ## Arguments
+ * * input:
+ *      a String of random words (lorem ipsum).
+ * 
+ */
+
 module.exports = upperCaser;
 
 function upperCaser(string){
